9bc91cbff8413e8773c830e3c7dcb51f4a25c41e
IslamTamjid <towhidulislam24@protonmail.com>
2025-08-14 04:16:50 +0600

    add AuthContext and AuthProvider for user authentication management

---
+++ app/contexts/AuthContext.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import { createClient } from "@/lib/client";
+import { User, Session } from "@supabase/supabase-js";
+import { createContext, useContext, useEffect, useState } from "react";
+
+interface AuthContextType {
+    user: User | null;
+    session: Session | null;
+    loading: boolean;
+    signOut: () => Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+    const [user, setUser] = useState<User | null>(null);
+    const [session, setSession] = useState<Session | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    const supabase = createClient();
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session);
+            setUser(session?.user ?? null);
+            setLoading(false);
+        })
+
+        const {
+            data: { subscription }
+        } = supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session);
+            setUser(session?.user ?? null);
+            setLoading(false);
+        });
+
+        return () => {
+            subscription.unsubscribe();
+        };
+        
+    }, [supabase.auth]);
+
+
+    async function signOut() {
+        await supabase.auth.signOut();
+        setUser(null);
+        setSession(null);
+        setLoading(false);
+    }
+
+    const value = {
+        user,
+        session,
+        loading,
+        signOut
+    };
+    return <AuthContext.Provider value={value}>
+        {children}
+    </AuthContext.Provider>;
+}
+
+export function useAuth() {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+    return context;
+}
